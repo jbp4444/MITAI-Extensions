b7e4e3dbb2ffc61592a2a7caaf7100f7b8b1480c
jbp4444 <jbp1@duke.edu>
2017-04-04 08:54:13 -0400

    .

--- src/ListOps.java
+++ src/ListOps.java
@@ -82,7 +82,7 @@ public final class ListOps extends AndroidNonvisibleComponent {
 	}
 
 	@SimpleFunction( description="Return a sorted list of strings" )
-	public YailList StringSort( YailList itemList ) {
+	public YailList MySimpleSort( YailList itemList ) {
 		String[] objList = itemList.toStringArray();
 		int n = objList.length;
 
